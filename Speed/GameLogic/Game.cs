fd536d856cac88dff59fe30151116327c66d4fd2
Mitchell Burke <dorgann2@gmail.com>
2022-03-29 19:17:26 -0600

    Made intial web sockets example (working), setup game logic folder and game hub for getting and sending web socket responses

---
+++ Speed/GameLogic/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace Speed.GameLogic
+{
+    public class Game
+    {
+        // add properties here
+
+        // TODO: move functions from game.cshtml.cs to here
+
+        public Game() { }
+    }
+}
---
+++ Speed/Hubs/GameHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using Speed.Pages;
+
+namespace Speed.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            // sends to all endpoints
+            //await Clients.All.SendAsync("ReceiveMessage", user, message);
+            // sends to the user that called the function
+            //await Clients.Caller.SendAsync("ReceiveMessage", user, message.ToUpper());
+            // sends to all other users except the person who called the function
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task EchoMessage(string user, string message)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message.ToUpper());
+        }
+    }
+}
